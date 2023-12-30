0175c205237c080d17de8fa5a657d82a8de90551
Ruba <ruba@Rubas-MacBook-Pro.local>
2023-12-30 11:47:30 -0800

    final commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 
-
+// CRUD VIDEO GAMES
 
 app.get('/api/videogames', async (req,res,next) => {
     try {
@@ -87,11 +87,75 @@ app.put('/api/videogames/:id', async (req,res,next) => {
 
 
 
-
+// CRUD BOARD GAMES
 
 app.get('/api/boardgames', async (req,res,next) => {
     try {
+        const SQL = `
+            SELECT *
+            FROM boardgames
+        `
+        const response = await client.query(SQL)
+        res.send(response.rows)
+    } catch (error) {
+        next(error)
+    }
+})
 
+app.get('/api/boardgames/:id', async (req,res,next) => {
+    try {
+        const SQL = `
+            SELECT *
+            FROM boardgames
+            WHERE id = $1
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.send(response.rows[0])
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.delete('/api/boardgames/:id', async (req,res,next) => {
+    try {
+        const SQL = `
+            DELETE
+            FROM boardgames
+            WHERE id = $1
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.post('/api/boardgames', async (req,res,next) => {
+    console.log(req.body)
+    try {
+        const SQL = `
+        INSERT INTO boardgames (name, price, rating)
+        VALUES($1, $2, $3)
+        RETURNING *
+        `
+        const response = await client.query(SQL, [req.body.name, req.body.price, req.body.rating])
+        res.send(response.rows)
+    } catch (error) {
+        next(error)
+    }
+
+})
+
+app.put('/api/boardgames/:id', async (req,res,next) => {
+    try {
+        const SQL =`
+            UPDATE boardgames
+            SET name = $1, price = $2, rating = $3
+            WHERE id = $4
+            RETURNING *
+        `
+        const response = await client.query(SQL, [req.body.name, req.body.price, req.body.rating, req.params.id])
+        res.send(response.rows)
     } catch (error) {
         next(error)
     }
@@ -99,6 +163,7 @@ app.get('/api/boardgames', async (req,res,next) => {
 
 
 
+//
 
 
 
@@ -113,9 +178,13 @@ const start = async () => {
     CREATE TABLE boardgames(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100),
+        price INT,
         rating INT
     );
-    INSERT INTO boardgames (name, rating) VALUES ('Monopoly', 3);
+    INSERT INTO boardgames (name, price, rating) VALUES ('Monopoly', 15, 3);
+    INSERT INTO boardgames (name, price, rating) VALUES ('Scrabble', 40, 3);
+    INSERT INTO boardgames (name, price, rating) VALUES ('Clue', 10, 3);
+    INSERT INTO boardgames (name, price, rating) VALUES ('Candy Land', 13, 3);
 
     CREATE TABLE videogames(
         id SERIAL PRIMARY KEY,
